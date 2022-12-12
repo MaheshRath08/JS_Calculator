c0c056771bb552014ad4442f33ae3a49a8a2b00f
MaheshRath08 <maheshrath.2002@gmail.com>
2022-12-13 01:49:44 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let keys = document.querySelectorAll(".keys")
+let display = document.querySelector(".display")
+let value = ""
+
+keys.forEach(key=>{
+    key.addEventListener("click", ()=>{
+        let inp = key.innerHTML
+        calculation(inp)
+    })
+})
+
+function calculation(e){
+    if(e=="c"){
+        value = ""
+    }else if(e=="←"){
+        value = value.slice(0, - 1)
+    }else if(e=="="){
+        let ans = eval(value)
+        value += e
+        value += ans
+    }else{
+        value += e
+    }
+    display.textContent = value
+}
